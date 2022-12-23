9ea23e9c71a73800cb98d7d7c94e174f09be93e2
luizaugustoaliare <luiz.augusto@aliare.co>
2022-12-22 21:47:59 -0300

    feat(Products): :sparkles: event when click button add cart
    emit event when click button add cart and your tests

--- Módulo 2/components/ProductCard.unit.spec.js
+++ Módulo 2/components/ProductCard.unit.spec.js
@@ -2,8 +2,25 @@ import { mount } from '@vue/test-utils';
 import ProductCard from './ProductCard';
 import { makeServer } from '@/miragejs/server';
 
+let server;
+const mountProductCard = () => {
+  const product = server.create('product', {
+    title: 'Relógio bonito',
+    price: '$23.00',
+    image:
+      'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  });
+  return {
+    wrapper: mount(ProductCard, {
+      propsData: {
+        product,
+      },
+    }),
+    product,
+  };
+};
+
 describe('ProductCard - unit', () => {
-  let server;
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
   });
@@ -12,18 +29,18 @@ describe('ProductCard - unit', () => {
   });
 
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '$23.00',
-          image:
-            'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        }),
-      },
-    });
+    const { wrapper } = mountProductCard();
 
     expect(wrapper.vm).toBeDefined();
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should emit the event addToCart with product object when button gets click', async () => {
+    const { wrapper, product } = mountProductCard();
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted().addToCart).toBeTruthy();
+    expect(wrapper.emitted().addToCart.length).toBe(1);
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
   });
 });
