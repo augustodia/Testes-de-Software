4f41ed45131fafbf467cc7e1d72ef08f676638be
luizaugustoaliare <luiz.augusto@aliare.co>
2022-12-25 20:08:08 -0300

    test(Search): :test_tube: Tests from search implementation
    made new tests for the research component

---
+++ Módulo 2/components/SearchComponent.unit.spec.js
@@ -0,0 +1,32 @@
+import { mount } from '@vue/test-utils';
+import SearchComponent from '@/components/SearchComponent';
+
+describe('Search - unit', () => {
+  it('should mount the component', () => {
+    const wrapper = mount(SearchComponent);
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should emit search event when form is submitted', async () => {
+    const wrapper = mount(SearchComponent);
+    const term = 'termo para busca';
+    await wrapper.find('input[type="search"]').setValue(term);
+    await wrapper.find('form').trigger('submit');
+
+    expect(wrapper.emitted().doSearch).toBeTruthy();
+    expect(wrapper.emitted().doSearch.length).toBe(1);
+    expect(wrapper.emitted().doSearch[0]).toEqual([{ term }]);
+  });
+
+  it('should emit search event when search input is cleared', async () => {
+    const wrapper = mount(SearchComponent);
+    const term = 'termo para busca';
+    const input = wrapper.find('input[type="search"]');
+    await input.setValue(term);
+    await input.setValue('');
+
+    expect(wrapper.emitted().doSearch).toBeTruthy();
+    expect(wrapper.emitted().doSearch.length).toBe(1);
+    expect(wrapper.emitted().doSearch[0]).toEqual([{ term: '' }]);
+  });
+});
